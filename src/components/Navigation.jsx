85cc7cc8593c753a5b8cfe3935089e5e0b377325
Shifa Zakir Khan <shifa24khan@gmail.com>
2023-07-03 17:26:52 +0530

    first commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div>
+        <img src="/images/brand_logo.png"></img>
+      </div>
+
+      <ul>
+        <li>Menu</li>
+        <li>Location</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
